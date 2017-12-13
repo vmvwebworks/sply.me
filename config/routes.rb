e8dd96c7b9907847fd4da6b38f5f5ee627a15944
Marcos <vmvwebworks@gmail.com>
2017-12-13 22:26:58 +0100

    wip devise omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   mount Hyperloop::Engine => '/hyperloop'
+  
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
