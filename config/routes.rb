f6dfce71dde93c8b9a09b82e534725a83ec3fe7e
Marcos <vmvwebworks@gmail.com>
2017-12-13 21:34:29 +0100

    wip devise hyperloop

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount Hyperloop::Engine => '/hyperloop'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'hyperloop#helloworld'
+  root 'home#index'
 end
